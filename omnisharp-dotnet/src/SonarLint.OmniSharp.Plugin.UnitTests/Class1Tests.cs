caa2d1bfba8dc193d4466dacbf5089685783564f
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-06-14 13:02:06 +0100

    [DO NOT COMMIT] - added dummy code and test

---
+++ omnisharp-dotnet/src/SonarLint.OmniSharp.Plugin.UnitTests/Class1Tests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SonarLint.OmniSharp.Plugin.UnitTests
+{
+    [TestClass]
+    public class Class1Tests
+    {
+        [TestMethod]
+        public void TestDoStuff()
+        {
+            var testSubject = new Class1();
+
+            testSubject.DoStuff(1, 2).Should().Be(3);
+        }
+    }
+}
