54e233e116fd27a40a771323d46d67fcce8ed42b
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-06-14 13:02:06 +0100

    [DO NOT COMMIT] - added dummy code and test

---
+++ omnisharp-dotnet/src/SonarLint.OmniSharp.Plugin/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SonarLint.OmniSharp.Plugin
+{
+    internal class Class1
+    {
+        // TODO
+
+        public int DoStuff(int a, int b)
+        {
+            if (a > 100)
+            {
+                throw new ArgumentException();
+            }
+
+            return a + b;
+        }
+    }
+}
