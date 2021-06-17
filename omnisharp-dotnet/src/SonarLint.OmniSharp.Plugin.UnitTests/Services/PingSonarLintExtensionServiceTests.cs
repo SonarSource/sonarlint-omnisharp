17e059a3faa2a8e9b465ac058f982774b8dfafa9
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-06-17 18:54:41 +0100

    Added tests

---
+++ omnisharp-dotnet/src/SonarLint.OmniSharp.Plugin.UnitTests/Services/PingSonarLintExtensionServiceTests.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * SonarOmnisharp
+ * Copyright (C) 2021-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OmniSharp.Mef;
+using SonarLint.OmniSharp.Plugin.Services;
+using SonarLint.VisualStudio.Integration.UnitTests;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Threading.Tasks;
+
+namespace SonarLint.OmniSharp.Plugin.UnitTests.Services
+{
+    [TestClass]
+    public class PingSonarLintExtensionServiceTests
+    {
+        [TestMethod, Ignore]
+        public void MefCtor_CheckIsExported()
+        {
+            // For some reason our normal MEF test helper doesn't work with this type.
+            // To illustrate the problem, the TypeCatalog should discover one "Part" on the
+            // type (which is what happens with e.g. ISonarAnalyzerCodeActionProvider in this assembly).
+            // It doesn't discover any.
+            // The only obvious difference with PingSonarLintExtensionService is that it is using a 
+            // custom Export attribute type, rather than just "[Export(...)]"
+            // However, the new service *is* discovered when running in OmniSharp.
+            var typeCatalog = new TypeCatalog(typeof(PingSonarLintExtensionService));
+            var compositionElement = (ICompositionElement)typeCatalog;
+            compositionElement.DisplayName.Should().NotContain("Empty");
+
+            MefTestHelpers.CheckTypeCanBeImported<PingSonarLintExtensionService, IRequestHandler>(null, null);
+        }
+
+        [TestMethod]
+        public async Task TestHandle()
+        {
+            var testSubject = new PingSonarLintExtensionService();
+            var response = await testSubject.Handle(new PingRequest());
+            response.Message.Should().NotBeNullOrEmpty();
+        }
+    }
+}
