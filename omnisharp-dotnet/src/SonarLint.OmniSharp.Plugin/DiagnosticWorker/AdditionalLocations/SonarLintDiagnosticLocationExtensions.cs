a50d1747827b990111ae4fd871846161654650f9
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2021-06-24 15:06:19 +0200

    Support secondary locations (#67)

---
+++ omnisharp-dotnet/src/SonarLint.OmniSharp.Plugin/DiagnosticWorker/AdditionalLocations/SonarLintDiagnosticLocationExtensions.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * SonarOmnisharp
+ * Copyright (C) 2021-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace SonarLint.OmniSharp.Plugin.DiagnosticWorker.AdditionalLocations
+{
+    internal static class SonarLintDiagnosticLocationExtensions
+    {
+        public static ICodeLocation[] ToAdditionalLocations(this Diagnostic diagnostic)
+        {
+            var additionalLocations = new List<ICodeLocation>();
+
+            for (var i = 0; i < diagnostic.AdditionalLocations.Count; i++)
+            {
+                var location = diagnostic.AdditionalLocations[i];
+                var text = GetLocationMessage(diagnostic, i);
+                var additionalLocation = location.ToAdditionalLocation(text);
+
+                additionalLocations.Add(additionalLocation);
+            }
+
+            return additionalLocations.ToArray();
+        }
+
+        /// <summary>
+        /// Based on sonar-dotnet logic:
+        /// https://github.com/SonarSource/sonar-dotnet/blob/master/analyzers/src/SonarAnalyzer.Common/Common/SecondaryLocation.cs#L55
+        /// </summary>
+        private static string GetLocationMessage(Diagnostic diagnostic, int i) => diagnostic.Properties.GetValueOrDefault(i.ToString());
+
+        private static CodeLocation ToAdditionalLocation(this Location location, string text)
+        {
+            var span = location.GetMappedLineSpan();
+
+            return new CodeLocation
+            {
+                FileName = span.Path,
+                Line = span.StartLinePosition.Line,
+                Column = span.StartLinePosition.Character,
+                EndLine = span.EndLinePosition.Line,
+                EndColumn = span.EndLinePosition.Character,
+                Text = text
+            };
+        }
+    }
+}
--- omnisharp-dotnet/src/SonarLint.OmniSharp.Plugin/DiagnosticWorker/OmniSharp/DiagnosticExtensions.cs
+++ omnisharp-dotnet/src/SonarLint.OmniSharp.Plugin/DiagnosticWorker/OmniSharp/DiagnosticExtensions.cs
@@ -10,11 +10,12 @@ using OmniSharp.Roslyn.CSharp.Services.Diagnostics;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using SonarLint.OmniSharp.Plugin.DiagnosticWorker.AdditionalLocations;
 
 namespace OmniSharp.Helpers
 {
     /// <summary>
-    /// Copied unchanged from OmniSharp.Helpers.DiagnosticExtensions
+    /// Copied from OmniSharp.Helpers.DiagnosticExtensions and modified to support secondary locations
     /// https://github.com/OmniSharp/omnisharp-roslyn/blob/master/src/OmniSharp.Roslyn.CSharp/Helpers/DiagnosticExtensions.cs
     /// </summary>
     internal static class DiagnosticExtensions
@@ -22,10 +23,11 @@ namespace OmniSharp.Helpers
         private static readonly ImmutableHashSet<string> _tagFilter =
             ImmutableHashSet.Create("Unnecessary", "Deprecated");
 
-        internal static DiagnosticLocation ToDiagnosticLocation(this Diagnostic diagnostic)
+        internal static SonarLintDiagnosticLocation ToDiagnosticLocation(this Diagnostic diagnostic)
         {
             var span = diagnostic.Location.GetMappedLineSpan();
-            return new DiagnosticLocation
+            
+            return new SonarLintDiagnosticLocation
             {
                 FileName = span.Path,
                 Line = span.StartLinePosition.Line,
@@ -38,11 +40,12 @@ namespace OmniSharp.Helpers
                     .Descriptor.CustomTags
                     .Where(x => _tagFilter.Contains(x))
                     .ToArray(),
-                Id = diagnostic.Id
+                Id = diagnostic.Id,
+                AdditionalLocations = diagnostic.ToAdditionalLocations()
             };
         }
 
-        internal static IEnumerable<DiagnosticLocation> DistinctDiagnosticLocationsByProject(this IEnumerable<DocumentDiagnostics> documentDiagnostic)
+        internal static IEnumerable<SonarLintDiagnosticLocation> DistinctDiagnosticLocationsByProject(this IEnumerable<DocumentDiagnostics> documentDiagnostic)
         {
             return documentDiagnostic
                 .SelectMany(x => x.Diagnostics, (parent, child) => (projectName: parent.ProjectName, diagnostic: child))
