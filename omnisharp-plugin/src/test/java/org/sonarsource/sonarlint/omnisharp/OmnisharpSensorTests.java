4f8925ddf3f12306b6331ca275c6d05d8bab274b
Julien HENRY <julien.henry@sonarsource.com>
2021-06-29 16:24:21 +0200

    Support secondary locations

--- omnisharp-plugin/src/test/java/org/sonarsource/sonarlint/omnisharp/OmnisharpSensorTests.java
+++ omnisharp-plugin/src/test/java/org/sonarsource/sonarlint/omnisharp/OmnisharpSensorTests.java
@@ -225,6 +225,7 @@ class OmnisharpSensorTests {
     Consumer<OmnisharpDiagnostic> issueConsummer = captor.getValue();
 
     OmnisharpDiagnostic diag = new OmnisharpDiagnostic();
+    diag.filename = filePath.toString();
     diag.id = "S12345";
     diag.line = 1;
     diag.column = 1;
@@ -240,6 +241,80 @@ class OmnisharpSensorTests {
       i -> i.primaryLocation().textRange().end().line(),
       i -> i.primaryLocation().textRange().end().lineOffset())
       .containsOnly(tuple(ruleKey, file, "Don't do this", 1, 0, 1, 4));
+  }
+
+  @Test
+  void processSecondaryLocations() throws Exception {
+    SensorContextTester sensorContext = SensorContextTester.create(baseDir);
+
+    RuleKey ruleKey = RuleKey.of(CSharpPlugin.REPOSITORY_KEY, "S12345");
+    sensorContext.setActiveRules(new ActiveRulesBuilder().addRule(new NewActiveRule.Builder().setRuleKey(ruleKey).build()).build());
+
+    Path filePath = baseDir.resolve("Foo.cs");
+    String content = "Console.WriteLine(\"Hello \n Woooooooooooooooooooooooooooorld!\");";
+    Files.write(filePath, content.getBytes(StandardCharsets.UTF_8));
+
+    InputFile file = TestInputFileBuilder.create("", "Foo.cs")
+      .setModuleBaseDir(baseDir)
+      .setLanguage(CSharpPlugin.LANGUAGE_KEY)
+      .setCharset(StandardCharsets.UTF_8)
+      .initMetadata(content)
+      .build();
+    sensorContext.fileSystem().add(file);
+
+    ArgumentCaptor<Consumer<OmnisharpDiagnostic>> captor = ArgumentCaptor.forClass(Consumer.class);
+
+    underTest.execute(sensorContext);
+
+    verify(mockProtocol).codeCheck(eq(filePath.toFile()), captor.capture());
+
+    Consumer<OmnisharpDiagnostic> issueConsummer = captor.getValue();
+
+    OmnisharpDiagnostic diag = new OmnisharpDiagnostic();
+    diag.filename = filePath.toString();
+    diag.id = "S12345";
+    diag.line = 1;
+    diag.column = 1;
+    diag.endLine = 1;
+    diag.endColumn = 5;
+    diag.text = "Don't do this";
+
+    OmnisharpDiagnosticLocation secondary1 = new OmnisharpDiagnosticLocation();
+    secondary1.filename = filePath.toString();
+    secondary1.line = 2;
+    secondary1.column = 3;
+    secondary1.endLine = 2;
+    secondary1.endColumn = 5;
+    secondary1.text = "Secondary 1";
+
+    OmnisharpDiagnosticLocation secondary2 = new OmnisharpDiagnosticLocation();
+    secondary2.filename = filePath.toString();
+    secondary2.line = 2;
+    secondary2.column = 8;
+    secondary2.endLine = 2;
+    secondary2.endColumn = 10;
+    secondary2.text = "Secondary 2";
+
+    diag.additionalLocations = new OmnisharpDiagnosticLocation[] {secondary1, secondary2};
+
+    issueConsummer.accept(diag);
+
+    assertThat(sensorContext.allIssues()).extracting(Issue::ruleKey, i -> i.primaryLocation().inputComponent(), i -> i.primaryLocation().message(),
+      i -> i.primaryLocation().textRange().start().line(),
+      i -> i.primaryLocation().textRange().start().lineOffset(),
+      i -> i.primaryLocation().textRange().end().line(),
+      i -> i.primaryLocation().textRange().end().lineOffset())
+      .containsOnly(tuple(ruleKey, file, "Don't do this", 1, 0, 1, 4));
+
+    Issue issue = sensorContext.allIssues().iterator().next();
+    assertThat(issue.flows()).hasSize(2);
+    assertThat(issue.flows()).allMatch(f -> f.locations().size() == 1);
+    assertThat(issue.flows().get(0).locations()).extracting(l -> l.inputComponent(), l -> l.message(),
+      l -> l.textRange().start().line(),
+      l -> l.textRange().start().lineOffset(),
+      l -> l.textRange().end().line(),
+      l -> l.textRange().end().lineOffset())
+      .containsOnly(tuple(file, "Secondary 1", 2, 2, 2, 4));
   }
 
 }
