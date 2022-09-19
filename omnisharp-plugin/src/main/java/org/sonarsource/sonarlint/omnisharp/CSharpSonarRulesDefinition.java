ab8d0a0febce6865b904c6dd7868cd2966bba079
Julien HENRY <julien.henry@sonarsource.com>
2022-09-19 16:49:26 +0200

    Update embedded C# analyzer to 8.45
    Fixes #132

--- omnisharp-plugin/src/main/java/org/sonarsource/sonarlint/omnisharp/CSharpSonarRulesDefinition.java
+++ omnisharp-plugin/src/main/java/org/sonarsource/sonarlint/omnisharp/CSharpSonarRulesDefinition.java
@@ -21,18 +21,28 @@ package org.sonarsource.sonarlint.omnisharp;
 
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
+
+import com.google.gson.reflect.TypeToken;
 import org.sonar.api.SonarRuntime;
+import org.sonar.api.rule.RuleStatus;
 import org.sonar.api.rules.RuleType;
 import org.sonar.api.scanner.ScannerSide;
+import org.sonar.api.server.debt.DebtRemediationFunction;
+import org.sonar.api.server.rule.RuleParamType;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.api.server.rule.RulesDefinitionXmlLoader;
 import org.sonar.api.utils.Version;
@@ -47,9 +57,12 @@ import static org.sonarsource.sonarlint.omnisharp.OmnisharpPlugin.REPOSITORY_NAM
 @ScannerSide
 public class CSharpSonarRulesDefinition implements RulesDefinition {
   private static final Gson GSON = new Gson();
-  private static final String RULES_XML = "/org/sonar/plugins/csharp/rules.xml";
+  private static final String RESOURCES_DIRECTORY = "/org/sonar/plugins/csharp/";
+  private static final String METADATA_SUFFIX = "_c#";
 
   private final boolean isOwaspByVersionSupported;
+  private final boolean isAddPciDssSupported;
+  private final boolean isASVSSupported;
 
   private static String getSonarWayJsonPath() {
     return "org/sonar/plugins/" + PLUGIN_KEY + "/Sonar_way_profile.json";
@@ -57,6 +70,8 @@ public class CSharpSonarRulesDefinition implements RulesDefinition {
 
   public CSharpSonarRulesDefinition(SonarRuntime sonarRuntime) {
     this.isOwaspByVersionSupported = sonarRuntime.getApiVersion().isGreaterThanOrEqual(Version.create(9, 3));
+    this.isAddPciDssSupported = sonarRuntime.getApiVersion().isGreaterThanOrEqual(Version.create(9, 5));
+    this.isASVSSupported = sonarRuntime.getApiVersion().isGreaterThanOrEqual(Version.create(9, 9));
   }
 
   @Override
@@ -65,80 +80,147 @@ public class CSharpSonarRulesDefinition implements RulesDefinition {
       .createRepository(REPOSITORY_KEY, LANGUAGE_KEY)
       .setName(REPOSITORY_NAME);
 
-    RulesDefinitionXmlLoader loader = new RulesDefinitionXmlLoader();
-    withRulesXml(reader -> loader.load(repository, reader));
+    Type ruleListType = new TypeToken<List<Rule>>() {
+    }.getType();
+    List<Rule> rules = GSON.fromJson(readResource("Rules.json"), ruleListType);
+    for (Rule rule : rules) {
+      NewRule newRule = repository.createRule(rule.id);
+      configureRule(newRule, loadMetadata(rule.id), rule.parameters);
+      newRule.setHtmlDescription(readResource(rule.id + METADATA_SUFFIX + ".html"));
+    }
 
-    setupHotspotRules(repository.rules());
     activeDefaultRules(repository.rules());
 
     repository.done();
   }
 
-  public static void withRulesXml(Consumer<InputStreamReader> consumer) {
-    try (InputStreamReader reader = new InputStreamReader(CSharpSonarRulesDefinition.class.getResourceAsStream(RULES_XML), StandardCharsets.UTF_8)) {
-      consumer.accept(reader);
-    } catch (IOException e) {
-      throw new IllegalStateException("Error reading rules.xml", e);
+  private void configureRule(NewRule rule, RuleMetadata metadata, RuleParameter[] parameters) {
+    rule
+            .setName(metadata.title)
+            .setType(RuleType.valueOf(metadata.type))
+            .setStatus(RuleStatus.valueOf(metadata.status.toUpperCase(Locale.ROOT)))
+            .setSeverity(metadata.defaultSeverity.toUpperCase(Locale.ROOT))
+            .setTags(metadata.tags);
+    if (metadata.remediation != null) { // Hotspots do not have remediation
+      rule.setDebtRemediationFunction(metadata.remediation.remediationFunction(rule));
+      rule.setGapDescription(metadata.remediation.linearDesc);
+    }
+
+    for (RuleParameter param : parameters) {
+      rule.createParam(param.key)
+              .setType(RuleParamType.parse(param.type))
+              .setDescription(param.description)
+              .setDefaultValue(param.defaultValue);
+    }
+
+    addSecurityStandards(rule, metadata.securityStandards);
+  }
+
+  private void addSecurityStandards(NewRule rule, SecurityStandards securityStandards) {
+    addASVS(rule, securityStandards);
+    addCwe(rule, securityStandards);
+    addOwasp(rule, securityStandards);
+    addPciDss(rule, securityStandards);
+  }
+
+  private void addASVS(NewRule rule, SecurityStandards securityStandards){
+    if (!isASVSSupported) {
+      return;
+    }
+
+    if (securityStandards.asvs4_0.length > 0){
+      rule.addOwaspAsvs(OwaspAsvsVersion.V4_0, securityStandards.asvs4_0);
     }
   }
 
-  private static String getRuleJson(String ruleKey) {
-    return "/org/sonar/plugins/csharp/" + ruleKey + "_c#.json";
+  private void addCwe(NewRule rule, SecurityStandards securityStandards) {
+    rule.addCwe(securityStandards.cwe);
   }
 
-  private void setupHotspotRules(Collection<NewRule> rules) {
-    Map<NewRule, RuleMetadata> allRuleMetadata = rules.stream()
-      .collect(Collectors.toMap(rule -> rule, rule -> readRuleMetadata(rule.key())));
-
-    Set<NewRule> hotspotRules = getHotspotRules(allRuleMetadata);
-
-    allRuleMetadata.forEach(this::updateSecurityStandards);
-    hotspotRules.forEach(rule -> rule.setType(RuleType.SECURITY_HOTSPOT));
-  }
-
-  private static void activeDefaultRules(Collection<NewRule> rules) {
-    Set<String> activeKeys = BuiltInQualityProfileJsonLoader.loadActiveKeysFromJsonProfile(getSonarWayJsonPath());
-    rules.forEach(rule -> rule.setActivatedByDefault(activeKeys.contains(rule.key())));
-  }
-
-  private static Set<NewRule> getHotspotRules(Map<NewRule, RuleMetadata> allRuleMetadata) {
-    return allRuleMetadata.entrySet()
-      .stream()
-      .filter(entry -> entry.getValue().isSecurityHotspot())
-      .map(Map.Entry::getKey)
-      .collect(Collectors.toSet());
-  }
-
-  private void updateSecurityStandards(NewRule rule, RuleMetadata ruleMetadata) {
-    for (String s : ruleMetadata.securityStandards.owasp2017) {
+  private void addOwasp(NewRule rule, SecurityStandards securityStandards) {
+    for (String s : securityStandards.owasp2017) {
       rule.addOwaspTop10(RulesDefinition.OwaspTop10.valueOf(s));
     }
+
     if (isOwaspByVersionSupported) {
-      for (String s : ruleMetadata.securityStandards.owasp2021) {
+      for (String s : securityStandards.owasp2021) {
         rule.addOwaspTop10(RulesDefinition.OwaspTop10Version.Y2021, RulesDefinition.OwaspTop10.valueOf(s));
       }
     }
-    rule.addCwe(ruleMetadata.securityStandards.cwe);
   }
 
-  private static RuleMetadata readRuleMetadata(String ruleKey) {
-    String resourcePath = getRuleJson(ruleKey);
-    try (InputStream stream = AbstractRulesDefinition.class.getResourceAsStream(resourcePath)) {
-      return GSON.fromJson(new InputStreamReader(stream, StandardCharsets.UTF_8), RuleMetadata.class);
-    } catch (IOException e) {
-      throw new IllegalStateException("Failed to read: " + resourcePath, e);
+  private void addPciDss(NewRule rule, SecurityStandards securityStandards) {
+    if (!isAddPciDssSupported) {
+      return;
+    }
+
+    if (securityStandards.pciDss3_2.length > 0){
+      rule.addPciDss(PciDssVersion.V3_2, securityStandards.pciDss3_2);
+    }
+
+    if (securityStandards.pciDss4_0.length > 0){
+      rule.addPciDss(PciDssVersion.V4_0, securityStandards.pciDss4_0);
     }
   }
 
-  private static class RuleMetadata {
-    private static final String SECURITY_HOTSPOT = "SECURITY_HOTSPOT";
+  private RuleMetadata loadMetadata(String id) {
+    return GSON.fromJson(readResource(id + METADATA_SUFFIX + ".json"), RuleMetadata.class);
+  }
 
-    String sqKey;
+  private String readResource(String name) {
+    InputStream stream = getResourceAsStream(RESOURCES_DIRECTORY + name);
+    if (stream == null) {
+      throw new IllegalStateException("Resource does not exist: " + name);
+    }
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+      return reader.lines().collect(Collectors.joining("\n"));
+    } catch (IOException e) {
+      throw new IllegalStateException("Failed to read: " + name, e);
+    }
+  }
+
+  // Extracted for testing
+  InputStream getResourceAsStream(String name) {
+    return getClass().getResourceAsStream(name);
+  }
+
+  private static class Rule {
+    String id;
+    RuleParameter[] parameters;
+  }
+
+  private static class RuleParameter {
+    String key;
+    String description;
     String type;
-    SecurityStandards securityStandards = new SecurityStandards();
+    String defaultValue;
+  }
 
-    boolean isSecurityHotspot() {
-      return SECURITY_HOTSPOT.equals(type);
+  static class RuleMetadata {
+    String title;
+    String status;
+    String type;
+    String[] tags;
+    String defaultSeverity;
+    Remediation remediation;
+    SecurityStandards securityStandards = new SecurityStandards();
+  }
+
+  private static class Remediation {
+    String func;
+    String constantCost;
+    String linearDesc;
+    String linearOffset;
+    String linearFactor;
+
+    public DebtRemediationFunction remediationFunction(NewRule rule) {
+      if (func.startsWith("Constant")) {
+        return rule.debtRemediationFunctions().constantPerIssue(constantCost);
+      } else if ("Linear".equals(func)) {
+        return rule.debtRemediationFunctions().linear(linearFactor);
+      } else {
+        return rule.debtRemediationFunctions().linearWithOffset(linearFactor, linearOffset);
+      }
     }
   }
 
@@ -151,5 +233,21 @@ public class CSharpSonarRulesDefinition implements RulesDefinition {
 
     @SerializedName("OWASP")
     String[] owasp2017 = {};
+
+    @SerializedName("PCI DSS 3.2")
+    String[] pciDss3_2 = {};
+
+    @SerializedName("PCI DSS 4.0")
+    String[] pciDss4_0 = {};
+
+    @SerializedName("ASVS 4.0")
+    String[] asvs4_0 = {};
   }
+
+
+  private static void activeDefaultRules(Collection<NewRule> rules) {
+    Set<String> activeKeys = BuiltInQualityProfileJsonLoader.loadActiveKeysFromJsonProfile(getSonarWayJsonPath());
+    rules.forEach(rule -> rule.setActivatedByDefault(activeKeys.contains(rule.key())));
+  }
+
 }
