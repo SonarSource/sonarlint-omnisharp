073164176c0a565bc3a861f5bcb40e4882ab9a79
Damien Urruty <damien.urruty@sonarsource.com>
2022-11-09 12:44:34 +0100

    Improve coverage

--- omnisharp-plugin/src/test/java/org/sonarsource/sonarlint/omnisharp/OmnisharpSensorTests.java
+++ omnisharp-plugin/src/test/java/org/sonarsource/sonarlint/omnisharp/OmnisharpSensorTests.java
@@ -22,22 +22,29 @@ package org.sonarsource.sonarlint.omnisharp;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
+import javax.annotation.Nullable;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.io.TempDir;
 import org.mockito.ArgumentCaptor;
+import org.sonar.api.batch.fs.InputComponent;
 import org.sonar.api.batch.fs.InputFile;
+import org.sonar.api.batch.fs.TextRange;
 import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
+import org.sonar.api.batch.rule.Severity;
 import org.sonar.api.batch.rule.internal.ActiveRulesBuilder;
 import org.sonar.api.batch.rule.internal.NewActiveRule;
 import org.sonar.api.batch.sensor.internal.DefaultSensorDescriptor;
 import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.batch.sensor.issue.Issue;
+import org.sonar.api.batch.sensor.issue.NewIssue;
+import org.sonar.api.batch.sensor.issue.NewIssueLocation;
 import org.sonar.api.config.Configuration;
 import org.sonar.api.rule.RuleKey;
 import org.sonar.api.utils.log.LogTesterJUnit5;
@@ -48,6 +55,10 @@ import org.sonarsource.sonarlint.omnisharp.protocol.Fix;
 import org.sonarsource.sonarlint.omnisharp.protocol.OmnisharpEndpoints;
 import org.sonarsource.sonarlint.omnisharp.protocol.QuickFix;
 import org.sonarsource.sonarlint.omnisharp.protocol.QuickFixEdit;
+import org.sonarsource.sonarlint.plugin.api.issue.NewInputFileEdit;
+import org.sonarsource.sonarlint.plugin.api.issue.NewQuickFix;
+import org.sonarsource.sonarlint.plugin.api.issue.NewSonarLintIssue;
+import org.sonarsource.sonarlint.plugin.api.issue.NewTextEdit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
@@ -55,6 +66,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -122,7 +134,7 @@ class OmnisharpSensorTests {
   }
 
   @Test
-  void noopIfNoFiles() throws Exception {
+  void noopIfNoFiles() {
     SensorContextTester sensorContext = SensorContextTester.create(baseDir);
 
     underTest.execute(sensorContext);
@@ -280,12 +292,12 @@ class OmnisharpSensorTests {
 
     verify(mockProtocol).codeCheck(eq(filePath.toFile()), captor.capture());
 
-    Consumer<Diagnostic> issueConsummer = captor.getValue();
+    Consumer<Diagnostic> issueConsumer = captor.getValue();
 
     Diagnostic diag = mock(Diagnostic.class);
     when(diag.getId()).thenReturn("SA12345");
 
-    issueConsummer.accept(diag);
+    issueConsumer.accept(diag);
 
     assertThat(sensorContext.allIssues()).isEmpty();
   }
@@ -315,7 +327,7 @@ class OmnisharpSensorTests {
 
     verify(mockProtocol).codeCheck(eq(filePath.toFile()), captor.capture());
 
-    Consumer<Diagnostic> issueConsummer = captor.getValue();
+    Consumer<Diagnostic> issueConsumer = captor.getValue();
 
     Diagnostic diag = mock(Diagnostic.class);
     when(diag.getFilename()).thenReturn(filePath.toString());
@@ -326,7 +338,7 @@ class OmnisharpSensorTests {
     when(diag.getEndColumn()).thenReturn(5);
     when(diag.getText()).thenReturn("Don't do this");
 
-    issueConsummer.accept(diag);
+    issueConsumer.accept(diag);
 
     assertThat(sensorContext.allIssues()).extracting(Issue::ruleKey, i -> i.primaryLocation().inputComponent(), i -> i.primaryLocation().message(),
         i -> i.primaryLocation().textRange().start().line(),
@@ -362,7 +374,7 @@ class OmnisharpSensorTests {
 
     verify(mockProtocol).codeCheck(eq(filePath.toFile()), captor.capture());
 
-    Consumer<Diagnostic> issueConsummer = captor.getValue();
+    Consumer<Diagnostic> issueConsumer = captor.getValue();
 
     Diagnostic diag = mock(Diagnostic.class);
     when(diag.getFilename()).thenReturn(anotherFilePath.toString());
@@ -373,7 +385,7 @@ class OmnisharpSensorTests {
     when(diag.getEndColumn()).thenReturn(5);
     when(diag.getText()).thenReturn("Don't do this");
 
-    issueConsummer.accept(diag);
+    issueConsumer.accept(diag);
 
     assertThat(sensorContext.allIssues()).isEmpty();
   }
@@ -404,7 +416,7 @@ class OmnisharpSensorTests {
 
     verify(mockProtocol).codeCheck(eq(filePath.toFile()), captor.capture());
 
-    Consumer<Diagnostic> issueConsummer = captor.getValue();
+    Consumer<Diagnostic> issueConsumer = captor.getValue();
 
     Diagnostic diag = mock(Diagnostic.class);
     when(diag.getFilename()).thenReturn(filePath.toString());
@@ -441,7 +453,7 @@ class OmnisharpSensorTests {
 
     when(diag.getAdditionalLocations()).thenReturn(new DiagnosticLocation[]{secondary1, secondary2, secondaryOnAnotherFile});
 
-    issueConsummer.accept(diag);
+    issueConsumer.accept(diag);
 
     Issue issue = sensorContext.allIssues().iterator().next();
     assertThat(issue.flows()).hasSize(2);
@@ -455,7 +467,6 @@ class OmnisharpSensorTests {
   }
 
   @Test
-  @Disabled("SensorContextTester is not providing a SonarLintIssue")
   void processQuickFixes() throws Exception {
     SensorContextTester sensorContext = SensorContextTester.create(baseDir);
 
@@ -475,13 +486,18 @@ class OmnisharpSensorTests {
       .build();
     sensorContext.fileSystem().add(file);
 
+    // SensorContextTester is not quick fix aware yet
+    sensorContext = spy(sensorContext);
+    var issue = new MockSonarLintIssue();
+    when(sensorContext.newIssue()).thenReturn(issue);
+
     ArgumentCaptor<Consumer<Diagnostic>> captor = ArgumentCaptor.forClass(Consumer.class);
 
     underTest.execute(sensorContext);
 
     verify(mockProtocol).codeCheck(eq(filePath.toFile()), captor.capture());
 
-    Consumer<Diagnostic> issueConsummer = captor.getValue();
+    Consumer<Diagnostic> issueConsumer = captor.getValue();
 
     Diagnostic diag = mock(Diagnostic.class);
     when(diag.getFilename()).thenReturn(filePath.toString());
@@ -492,29 +508,216 @@ class OmnisharpSensorTests {
     when(diag.getEndColumn()).thenReturn(5);
     when(diag.getText()).thenReturn("Don't do this");
 
-
     QuickFix qfWithOneFixAndOneEditSameFile = mockQuickFix("Quick Fix With One Edit Same File",
       mockFix(filePath,
-        mockEdit(1, 2, 3, 4, "")));
+        mockEdit(1, 2, 1, 4, "")));
 
     QuickFix qfWithTwoFixesAndTwoEditsSameFile = mockQuickFix("Quick Fix With Multiple Edits",
       mockFix(filePath,
-        mockEdit(1, 2, 3, 4, ""),
-        mockEdit(5, 6, 7, 8, "another")),
+        mockEdit(1, 2, 1, 4, ""),
+        mockEdit(1, 6, 1, 8, "another")),
       mockFix(filePath,
-        mockEdit(11, 12, 13, 14, ""),
-        mockEdit(15, 16, 17, 18, "another")));
+        mockEdit(2, 12, 2, 14, ""),
+        mockEdit(2, 16, 2, 18, "another")));
 
     QuickFix qfDifferentFile = mockQuickFix("Quick Fix Different File",
       mockFix(anotherFilePath,
-        mockEdit(1, 2, 3, 4, "")));
+        mockEdit(1, 2, 1, 4, "")));
 
     when(diag.getQuickFixes()).thenReturn(new QuickFix[]{qfWithOneFixAndOneEditSameFile, qfWithTwoFixesAndTwoEditsSameFile, qfDifferentFile});
 
-    issueConsummer.accept(diag);
+    issueConsumer.accept(diag);
 
-    Issue issue = sensorContext.allIssues().iterator().next();
-    /*assertThat(issue.getQuickFixes).hasSize(3);*/
+    var quickFixes = issue.getQuickFixes();
+    assertThat(quickFixes)
+      .extracting("message")
+      .containsExactly("Quick Fix With One Edit Same File", "Quick Fix With Multiple Edits", "Quick Fix Different File");
+    assertThat(quickFixes)
+      .flatExtracting("inputFileEdits")
+      .extracting("inputFile")
+      .containsExactly(file, file, file/* why ? */);
+    assertThat(quickFixes)
+      .flatExtracting("inputFileEdits")
+      .flatExtracting("textEdits")
+      .extracting("textRange.start.line", "textRange.start.lineOffset", "textRange.end.line", "textRange.end.lineOffset", "newText")
+      .containsExactly(tuple(1, 1, 1, 3, ""), tuple(1, 1, 1, 3, ""), tuple(1, 5, 1, 7, "another"), tuple(2, 11, 2, 13, ""), tuple(2, 15, 2, 17, "another"));
+  }
+
+  private static class MockSonarLintIssue implements NewSonarLintIssue, NewIssue {
+    private final List<MockSonarLintQuickFix> quickFixes = new ArrayList<>();
+
+    @Override
+    public NewIssue forRule(RuleKey ruleKey) {
+      return this;
+    }
+
+    @Override
+    public NewIssue gap(@Nullable Double aDouble) {
+      return this;
+    }
+
+    @Override
+    public NewIssue overrideSeverity(@Nullable Severity severity) {
+      return this;
+    }
+
+    @Override
+    public NewIssue at(NewIssueLocation newIssueLocation) {
+      return this;
+    }
+
+    @Override
+    public NewIssue addLocation(NewIssueLocation newIssueLocation) {
+      return this;
+    }
+
+    @Override
+    public NewIssue setQuickFixAvailable(boolean b) {
+      return this;
+    }
+
+    @Override
+    public NewIssue addFlow(Iterable<NewIssueLocation> iterable) {
+      return this;
+    }
+
+    @Override
+    public NewIssue addFlow(Iterable<NewIssueLocation> iterable, NewIssue.FlowType flowType, @Nullable String s) {
+      return this;
+    }
+
+    @Override
+    public NewIssueLocation newLocation() {
+      return new MockIssueLocation();
+    }
+
+    @Override
+    public void save() {
+      // no op
+    }
+
+    @Override
+    public NewIssue setRuleDescriptionContextKey(@Nullable String s) {
+      return this;
+    }
+
+    @Override
+    public NewQuickFix newQuickFix() {
+      return new MockSonarLintQuickFix();
+    }
+
+    @Override
+    public NewSonarLintIssue addQuickFix(NewQuickFix newQuickFix) {
+      quickFixes.add((MockSonarLintQuickFix) newQuickFix);
+      return this;
+    }
+
+    public List<MockSonarLintQuickFix> getQuickFixes() {
+      return quickFixes;
+    }
+
+    private static class MockSonarLintQuickFix implements NewQuickFix {
+      private String message;
+      private final List<MockSonarLintInputFileEdit> inputFileEdits = new ArrayList<>();
+
+      @Override
+      public NewQuickFix message(String message) {
+        this.message = message;
+        return this;
+      }
+
+      @Override
+      public NewInputFileEdit newInputFileEdit() {
+        return new MockSonarLintInputFileEdit();
+      }
+
+      @Override
+      public NewQuickFix addInputFileEdit(NewInputFileEdit newInputFileEdit) {
+        inputFileEdits.add((MockSonarLintInputFileEdit) newInputFileEdit);
+        return this;
+      }
+
+      public String getMessage() {
+        return message;
+      }
+
+      public List<MockSonarLintInputFileEdit> getInputFileEdits() {
+        return inputFileEdits;
+      }
+
+      private static class MockSonarLintInputFileEdit implements NewInputFileEdit {
+        private InputFile inputFile;
+        private final List<MockSonarLintTextEdit> textEdits = new ArrayList<>();
+
+        @Override
+        public NewInputFileEdit on(InputFile inputFile) {
+          this.inputFile = inputFile;
+          return this;
+        }
+
+        @Override
+        public NewTextEdit newTextEdit() {
+          return new MockSonarLintTextEdit();
+        }
+
+        @Override
+        public NewInputFileEdit addTextEdit(NewTextEdit newTextEdit) {
+          textEdits.add((MockSonarLintTextEdit) newTextEdit);
+          return this;
+        }
+
+        public InputFile getInputFile() {
+          return inputFile;
+        }
+
+        public List<MockSonarLintTextEdit> getTextEdits() {
+          return textEdits;
+        }
+
+        private static class MockSonarLintTextEdit implements NewTextEdit {
+          private TextRange textRange;
+          private String newText;
+
+          @Override
+          public NewTextEdit at(TextRange textRange) {
+            this.textRange = textRange;
+            return this;
+          }
+
+          @Override
+          public NewTextEdit withNewText(String newText) {
+            this.newText = newText;
+            return this;
+          }
+
+          public TextRange getTextRange() {
+            return textRange;
+          }
+
+          public String getNewText() {
+            return newText;
+          }
+        }
+      }
+    }
+  }
+
+  private static class MockIssueLocation implements NewIssueLocation {
+
+    @Override
+    public NewIssueLocation on(InputComponent inputComponent) {
+      return this;
+    }
+
+    @Override
+    public NewIssueLocation at(TextRange textRange) {
+      return this;
+    }
+
+    @Override
+    public NewIssueLocation message(String s) {
+      return this;
+    }
   }
 
 }
