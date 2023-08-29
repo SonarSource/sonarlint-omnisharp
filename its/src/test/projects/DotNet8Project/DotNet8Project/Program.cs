72da0bbaa52dc3c1bcc0089682da8e2536f20492
Tobias Hahnen <tobias.hahnen@sonarsource.com>
2023-08-29 12:01:19 +0200

    Project for .NET 8
    Even though .NET 8 is not yet released nor supported by the C# analyzer, this can be used later on

---
+++ its/src/test/projects/DotNet8Project/DotNet8Project/Program.cs
@@ -0,0 +1,10 @@
+﻿// simple method
+void print()
+{
+    Console.WriteLine("Hello World");
+}
+
+// non-compliant example
+var start = DateTime.Now;
+print();
+Console.WriteLine($"{(DateTime.Now - start).TotalMilliseconds} ms");
