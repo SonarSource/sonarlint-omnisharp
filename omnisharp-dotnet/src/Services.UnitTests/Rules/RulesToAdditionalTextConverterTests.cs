6cf020159202b7ba721108092386224712a9ee2d
Samir M <samir.marini@sonarsource.com>
2025-11-14 14:25:36 +0100

    SC-35440 scripted updated of SonarSource SA to SonarSource Sàrl

--- omnisharp-dotnet/src/Services.UnitTests/Rules/RulesToAdditionalTextConverterTests.cs
+++ omnisharp-dotnet/src/Services.UnitTests/Rules/RulesToAdditionalTextConverterTests.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * SonarOmnisharp
- * Copyright (C) 2021-2025 SonarSource SA
+ * Copyright (C) 2021-2025 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
